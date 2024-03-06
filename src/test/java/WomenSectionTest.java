ecdef3cbc799c244b82d7e98fe23a545534b5498
MichalSznauder <michal.sznauder@gmail.com>
2024-03-06 20:40:46 +0100

    vanilla test of prices in blouse product

--- src/test/java/WomenSectionTest.java
+++ src/test/java/WomenSectionTest.java
@@ -40,39 +40,89 @@ public static void setUpClass() {
     }
 
 @Test
- void pricesOnWomenSectionShouldNotBeEmpty(){
+ void pricesOnWomenSectionShouldNotBeEmpty() {
 
-driver.get("http://www.automationpractice.pl/index.php");
+    driver.get("http://www.automationpractice.pl/index.php");
 
-Assertions.assertThat(driver.getTitle()).isEqualTo("My Shop");
+    Assertions.assertThat(driver.getTitle()).isEqualTo("My Shop");
 
-driver.findElement(By.xpath("//li/a[@title='Women']")).click();
+    driver.findElement(By.xpath("//li/a[@title='Women']")).click();
 
 
-    List<String> productsPriceList =  driver.findElements(By.xpath("//span[@class = 'price product-price' and not(parent::*[@itemprop='offers'])]")).stream()
-       .map(el -> el.getAttribute("textContent").trim())
+    List<String> productsPriceList = driver.findElements(By.xpath("//span[@class = 'price product-price' and not(parent::*[@itemprop='offers'])]")).stream()
+            .map(el -> el.getAttribute("textContent").trim().substring(1))
             .collect(Collectors.toList());
 
     System.out.println(productsPriceList);
 
-var productsWithEmptyPrice = productsPriceList.stream()
-        .filter(el -> el.isEmpty())
-        .collect(Collectors.toList());
+    var productsWithEmptyPrice = productsPriceList.stream()
+            .filter(el -> el.isEmpty())
+            .collect(Collectors.toList());
 
-Assertions.assertThat(productsWithEmptyPrice).isEmpty();
+    Assertions.assertThat(productsWithEmptyPrice).isEmpty();
 
-var productsWithZeroPrice = productsPriceList.stream()
-        .anyMatch(el -> el.equals("0"));
+    var productsWithZeroPrice = productsPriceList.stream()
+            .anyMatch(el -> el.equals("0"));
 
 
-Assertions.assertThat(productsWithZeroPrice).isFalse();
-
-var productsWithDollar = productsPriceList.stream()
-        .allMatch(el -> el.contains("$"));
-
-Assertions.assertThat(productsWithDollar).isTrue();
+    Assertions.assertThat(productsWithZeroPrice).isFalse();
 
 }
 
+@Test
+public void pricesShouldBeInDollars() {
+
+    driver.get("http://www.automationpractice.pl/index.php");
+
+    Assertions.assertThat(driver.getTitle()).isEqualTo("My Shop");
+
+    driver.findElement(By.xpath("//li/a[@title='Women']")).click();
+
+
+    List<String> productsPriceList = driver.findElements(By.xpath("//span[@class = 'price product-price' and not(parent::*[@itemprop='offers'])]")).stream()
+            .map(el -> el.getAttribute("textContent").trim())
+            .collect(Collectors.toList());
+
+    System.out.println(productsPriceList);
+
+
+    var productsWithDollar = productsPriceList.stream()
+                .allMatch(el -> el.contains("$"));
+
+        Assertions.assertThat(productsWithDollar).isTrue();
+
+    }
+
+@Test
+    public void blouseShouldHaveProperNameAndPrice(){
+
+     String blouse = "Blouse";
+
+    driver.get("http://www.automationpractice.pl/index.php");
+
+    Assertions.assertThat(driver.getTitle()).isEqualTo("My Shop");
+
+    driver.findElement(By.id("search_query_top")).click();
+
+    driver.findElement(By.id("search_query_top")).sendKeys(blouse);
+
+    driver.findElement(By.name("submit_search")).click();
+
+var blouseProductName = driver.findElement(By.xpath("//h5[@itemprop='name'] /a[@class='product-name' and contains(text(), 'Blouse')] ")).getText();
+
+    System.out.println(blouseProductName);
+
+    Assertions.assertThat(blouseProductName.contains("Blouse")).isTrue();
+
+
+    var blouseProductPrice = driver.findElement(By.xpath("//div[@class='right-block']//div[@class='content_price']/span")).getText().substring(1);
+
+    var blouseProductPriceInt = Integer.parseInt(blouseProductPrice);
+
+    System.out.println(blouseProductPriceInt);
+    Assertions.assertThat(blouseProductPriceInt > 0).isTrue();
+
+
+}
 
 }
